da4c4a7182975d09933caa6baa688895a6e1dd02
indivisual <ennyennson@gmail.com>
2020-10-01 08:44:18 +0200

    Backupbundle download (#74)
    * add DownloadController

---
+++ Controller/DownloadController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Enhavo\Bundle\BackupBundle\Controller;
+
+use Enhavo\Bundle\BackupBundle\Backup\BackupManager;
+use Enhavo\Bundle\BackupBundle\Entity\BackupFile;
+use Enhavo\Bundle\BackupBundle\Utility\FileHelper;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class DownloadController extends AbstractController
+{
+    /** @var BackupManager */
+    private $backupManager;
+    
+    /** @var RepositoryInterface */
+    private $fileRepository;
+    
+    /** @var FileHelper */
+    private $fileHelper;
+
+    /**
+     * DownloadController constructor.
+     * @param BackupManager $backupManager
+     * @param RepositoryInterface $fileRepository
+     * @param FileHelper $fileHelper
+     */
+    public function __construct(BackupManager $backupManager, RepositoryInterface $fileRepository, FileHelper $fileHelper)
+    {
+        $this->backupManager = $backupManager;
+        $this->fileRepository = $fileRepository;
+        $this->fileHelper = $fileHelper;
+    }
+
+    public function downloadAction(Request $request)
+    {
+        $id = $request->get('id');
+        /** @var BackupFile $backupFile */
+        $backupFile = $this->fileRepository->find($id);
+
+        return new BinaryFileResponse($this->fileHelper->normalizeDirectory($backupFile->getPath()) . '/' . $backupFile->getName(),
+            200, [], true,
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+    }
+}
